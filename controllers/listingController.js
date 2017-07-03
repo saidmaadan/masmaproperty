63ba2dc11134aca85db7f551e25bd8785f64b8d8
Said Maadan <me@saidmaadan.com>
2017-07-03 13:17:32 -0500

    Implemented mixins and flash messages

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+const listing = mongoose.model('Listing');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
+
+exports.addListing = (req, res) => {
+  res.render('editListing', {title: 'Add Listing'});
+};
+
+exports.createListing = async (req, res) => {
+  // const listing = await new Listing(req.body).save(); //short cut
+  const listing = new Listing(req.body)
+  await listing.save();
+  req.flash('success', `Successfully Created ${listing.title}`);
+  res.redirect('/listing/${listing.slug}');
+};
+
+exports.getListings  = async (req, res) => {
+  const listings = await listing.find()
+  res.render('listings', {title: "Listings", listings });
+}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const listingController = require('../controllers/listingController');
 
+const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
-router.get('/', listingController.homePage);
+router.get('/', catchErrors(listingController.getListings));
+router.get('/listings', catchErrors(listingController.getListings));
+router.get('/add', listingController.addListing);
+router.post('/add', catchErrors(listingController.createListing));
 
 module.exports = router;
